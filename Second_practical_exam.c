268c93ac6efbf66d395976392844e25519d9f784
Vitor Cordeiro Paes Prieto <67298769+SonngCS@users.noreply.github.com>
2021-11-04 11:04:38 -0300

    Update Second_practical_exam.c

--- Second_practical_exam.c
+++ Second_practical_exam.c
@@ -1,7 +1,7 @@
 /*
 Universidade Católica de Santos
 Aluno: Vitor Cordeiro Paes Prieto
-Projeto de Algoritmos
+Projeto de Algoritmos - Atividade Prática 1
 */
 
 #include <stdio.h>
@@ -34,29 +34,13 @@ int main(void){
 }
 
 int economia(int num, char lista[100000][201]){
-    int len, cont = 0, aux_i;
+    int len, cont = 0;
 
     for(int i = 1; i < num; i++){
-        aux_i = i - 1;
         for(int j = 0, len = strlen(lista[i]); j < len; j++){
-            while(lista[aux_i][j] == ' '){
-                /*
-                laço while para encontrar o número (lista[i][j]) das linhas anteriores, caso ele tenha se repetido alguma vez
-                e em seu lugar haja espaços em branco, adicionados pelo comando if abaixo;
-                */
-                aux_i--;
-            }
-            
-            if(lista[aux_i][j] == lista[i][j] && (i > 1 ? lista[i - 1][j] == ' ': lista[i - 1][j] != ' ')){
-                /*
-                Caso o elemento da posição lista[i][j] seja igual ao elemento  da posição lista[aux_i][j], devemos verificar se i > 1. 
-                Se verdadeiro, a omissão só ocorrerá se o elemento da linha acima for uma espaço em branco. Caso o elemento da linha 
-                acima não seja um espaço em branco, só haverá omissão se i = 1;
-                */
+            if(lista[i - 1][j] == lista[i][j]){
                 cont++;
-                lista[i][j] = ' ';
             }else{
-                //se um dos elementos iniciais não puder ser omitido, não é preciso verificar se os demais podem, pois as omissões são contínuas.
                 break;
             }
         }
