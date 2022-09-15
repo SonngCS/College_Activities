9cfb3d540e2492cdf75013bc3d5dd1171a65b1c3
Vitor Cordeiro Paes Prieto <67298769+SonngCS@users.noreply.github.com>
2022-09-15 20:31:50 -0300

    Add files via upload

---
+++ Java/aula6/produtos/Produto.java
@@ -0,0 +1,26 @@
+package produtos;
+public abstract class Produto{
+    private String nome;
+    private double valor;
+
+    public void setNome(String nome2){
+        this.nome = nome2;
+    }
+
+    public void setValor(double valor2){
+        this.valor = valor2;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getValor(double valor2){
+        return valor;
+    }
+
+    public double getValorLiquido(){
+        return 0.0;
+    }
+
+}
